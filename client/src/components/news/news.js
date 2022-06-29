ec3c68fca7bb35b973cec36ff4c71a093b2f00f9
Melvin-Leo2000 <80947918+Melvin-Leo2000@users.noreply.github.com>
2022-06-29 23:44:53 +0800

    Update news.js

--- client/src/components/news/news.js
+++ client/src/components/news/news.js
@@ -19,7 +19,7 @@ useEffect(() => {
 
   return (
     <div ClassName="news">
-        {posts && posts.length && posts.map((p, index) => (
+        {posts && posts.length && posts.map(p => (
           <Post post={p} />
         ))}
     </div>
