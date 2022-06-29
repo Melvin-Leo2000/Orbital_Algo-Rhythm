49b534d5f1d262fd558b9af2511c39e295b87b31
Melvin-Leo2000 <80947918+Melvin-Leo2000@users.noreply.github.com>
2022-06-30 00:13:35 +0800

    Update post.js

--- models/post.js
+++ models/post.js
@@ -18,11 +18,7 @@ const PostSchema = new mongoose.Schema(
     name: {
       type: String,
       required: true,
-    },
-    categories: {
-      type: Array,
-      required: false,
-    },
+    }
   },
   { timestamps: true }
 );
