b0ad110eee10990302605211d6e801fec43fc37a
Melvin-Leo2000 <80947918+Melvin-Leo2000@users.noreply.github.com>
2022-06-30 15:51:13 +0800

    Update specificpost.js

--- client/src/components/body/specificpost/specificpost.js
+++ client/src/components/body/specificpost/specificpost.js
@@ -3,28 +3,21 @@ import "./specificpost.css"
 import { Link } from "react-router-dom";
 
 
-export default function Specificpost({post}) {
+export default function Specificpost({_id, title, createdAt, photo, name}) {
   return (
-    <div>
-    <Link to={`/post/${post._id}`} className='post'>
-          {post.photo && <img className="postImg" src={post.photo} alt="" />}
+    <Link to={`/post/${_id}`} className='post'>
+          {photo && <img className="postImg" src={photo} alt="" />}
       <div className="postInfo">
-      <div className="postCats">
-          {post.categories.map((c) => (
-            <span className="postCat">{c.name}</span>
-          ))}
-        </div>
       <span className="postTitle">
-        <h3>{post.title}</h3>
+        <h3>{title}</h3>
       </span>
       <hr />
-      <span className="postDate">{new Date(post.createdAt).toDateString()}</span>
+      <span className="postDate">{new Date(createdAt).toDateString()}</span>
       <p className="postDesc">
-      By: {post.name}
+      By: {name}
     </p>
     </div>
 
   </Link>
-  </div>
   )
 }
